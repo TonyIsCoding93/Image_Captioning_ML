1fa6521473f1387a322b7ac0b33c98130a44dfdf
Anthony <tonydye730@gmail.com>
2026-04-17 23:48:58 -0500

    Cleaned up code into Class structure and created extractFeatures method.

--- main.py
+++ main.py
@@ -1,20 +1,66 @@
+import numpy as np
+import os
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 
 
-def loadCaptions():
+class ImageCaptioningModel:
 
-    hashmap = {}
-    
-    with open("data/archive/captions.txt", "r") as file:
-        lines = file.readlines()
-        
-        for line in lines[1:]:
-            seperatedLine = line.strip().split(",", 1)
-            if seperatedLine[0] not in hashmap:
-                hashmap[seperatedLine[0]] = [seperatedLine[1]]
-            else:
-                hashmap[seperatedLine[0]].append(seperatedLine[1])
-    return hashmap
+    def __init__(self, captions_path, images_path):
+        self.captions_path = captions_path
+        self.images_path = images_path
+        self.captions = {}
+        self.images = {}
+    # function that loads our image files and captions into a dictionary
+    def loadCaptions(self):
+
+        hashmap = {}
+        #"data/archive/captions.txt"
+        with open(self.captions_path, "r") as file:
+            lines = file.readlines()
+            
+            for line in lines[1:]:
+                seperatedLine = line.strip().split(",", 1)
+                if seperatedLine[0] not in hashmap:
+                    hashmap[seperatedLine[0]] = [seperatedLine[1]]
+                else:
+                    hashmap[seperatedLine[0]].append(seperatedLine[1])
+        self.captions = hashmap
+
+    def extractFeatures(self):
+        #using pre-trained weights from imagenet and taking off fully connected layers
+        #to allow our to make classifications 
+        baseModel = VGG16(weights='imagenet', include_top=False, pooling='avg')
+        #we dont want to train this model. we just want to extract the features and numbers
+        #assocciated with them
+        baseModel.trainable = False
+        for filename in os.listdir(self.images_path):
+            #create full path so when we load the image it knows exactly where to get it from
+            full_path = os.path.join(self.images_path, filename)
+            #resize image to 224x224 which is what KGG16 works with
+            image = load_img(full_path, target_size=(224, 224))
+            #using keras, convert to numPy array
+            image = img_to_array(image)
+            #VGG16 is meant to process images by batches so here we add a batch number for processing
+            image = np.expand_dims(image, axis=0)
+            #normalize pixels so we have proper feature vectors
+            image = preprocess_input(image)
+            self.features[filename] = baseModel.predict(image, verbose=0) ##verbose to cancel progress bar
+
+        np.save('features.npy', self.features)
+
+
+
+
+
+
+
+
+model = ImageCaptioningModel("data/archive/captions.txt", "data/archive/Images/")
+model.loadCaptions()
+model.extractFeatures()
             
 
 
