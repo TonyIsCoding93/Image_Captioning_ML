af90d8fb802e16d38811c8f2e38c83420fc9056d
Anthony <tonydye730@gmail.com>
2026-05-02 16:44:25 -0500

    added back propegation to fix weights

--- main.py
+++ main.py
@@ -38,6 +38,10 @@ class LSTM:
         return exp_x / exp_x.sum(axis=0)
     
     def forward_pass(self, x, hidden_state, cell_state):
+
+        self.last_prev_hidden = hidden_state.copy()
+        self.last_prev_cell = cell_state.copy()
+        self.last_word_index = x
         #grab row from embed matrix and make it a column
         word_embed = self.embedding[x].reshape(-1, 1)
         #combing hidden state and word embedding into 1 long vertical vector
@@ -58,7 +62,83 @@ class LSTM:
         output = self.weight_vocab @ hidden_state + self.bias_vocab
         probabilities = self.to_probabilities(output)
 
+        self.last_combined = combined
+        self.last_forget = forget
+        self.last_input = input_gate
+        self.last_cell_update = cell_update
+        self.last_output = output_gate
+        self.last_cell_state = cell_state
+        self.last_hidden_state = hidden_state
+        self.last_probabilities = probabilities
+        
+
         return probabilities, hidden_state, cell_state
+    
+    def back_prop(self, target_index, learning_rate = 0.001):
+        #get the error by taking our predicted probabilities and subtracting 1 from the correct word
+        #this tells us how far off every word was from what it should be
+        error = self.last_probabilities.copy()
+        error[target_index] -= 1
+
+        #figure out how much the output layer weights contributed to the error
+        grad_weight_vocab = error @ self.last_hidden_state.T
+        grad_bias_vocab = error
+
+        #push the error backwards into the hidden state
+        grad_hidden = self.weight_vocab.T @ error
+
+        #trace error through the output gate using sigmoid derivative
+        tanh_cell = np.tanh(self.last_cell_state)
+        grad_output_gate = grad_hidden * tanh_cell * self.last_output * (1 - self.last_output)
+
+        #trace error into the cell state, our long term memory
+        grad_cell = grad_hidden * self.last_output * (1 - tanh_cell ** 2)
+
+        #trace error through forget gate. did it erase the wrong stuff?
+        grad_forget = grad_cell * self.last_prev_cell * self.last_forget * (1 - self.last_forget)
+
+        #trace error through input gate. did it let in the wrong stuff?
+        grad_input = grad_cell * self.last_cell_update * self.last_input * (1 - self.last_input)
+
+        #trace error through cell update. did it write the wrong info?
+        grad_cell_update = grad_cell * self.last_input * (1 - self.last_cell_update ** 2)
+
+        #calculate how much each gates weights need to change
+        grad_weight_forget = grad_forget @ self.last_combined.T
+        grad_bias_forget = grad_forget
+
+        grad_weight_input = grad_input @ self.last_combined.T
+        grad_bias_input = grad_input
+
+        grad_weight_cell = grad_cell_update @ self.last_combined.T
+        grad_bias_cell = grad_cell_update
+
+        grad_weight_output = grad_output_gate @ self.last_combined.T
+        grad_bias_output = grad_output_gate
+
+        #trace error all the way back to the word embedding
+        grad_combined = (self.weight_forget_gate.T @ grad_forget +
+                        self.weight_input.T @ grad_input +
+                        self.weight_cell.T @ grad_cell_update +
+                        self.weight_output.T @ grad_output_gate)
+        grad_embed = grad_combined[self.hidden_size:]
+
+        #update every single weight. nudge each one based on its gradient
+        self.weight_vocab -= learning_rate * grad_weight_vocab
+        self.bias_vocab -= learning_rate * grad_bias_vocab
+        self.weight_forget_gate -= learning_rate * grad_weight_forget
+        self.bias_forget_gate -= learning_rate * grad_bias_forget
+        self.weight_input -= learning_rate * grad_weight_input
+        self.bias_input -= learning_rate * grad_bias_input
+        self.weight_cell -= learning_rate * grad_weight_cell
+        self.bias_cell -= learning_rate * grad_bias_cell
+        self.weight_output -= learning_rate * grad_weight_output
+        self.bias_output -= learning_rate * grad_bias_output
+        self.embedding[self.last_word_index] -= learning_rate * grad_embed.flatten()
+
+        #calculate the loss so we can track if training is getting better
+        loss = -np.log(self.last_probabilities[target_index] + 1e-8)
+        return loss[0]
 
 class ImageCaptioningModel:
 
